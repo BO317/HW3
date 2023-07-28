7085daf569744f1403ad05fc4a08bb03d5920d9f
BO317 <85127586+BO317@users.noreply.github.com>
2023-07-28 12:22:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+# CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+
+CORS(app)
 
 
 users = {
